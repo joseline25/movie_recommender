bc15826b838eba12926247fcadfc31c76a5451d4
joseline25 <youjoseline@gmail.com>
2024-02-22 18:15:02 +0100

    first views and recommendations

--- movierecommender/urls.py
+++ movierecommender/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     # route is a string contains a URL pattern
+    
+    # list of movies in the database
+    path(route='movies/', view=views.movie_recommendation_view, name='recommendations'),
+    path(route='', view=views.movie_recommendation_view, name='recommendations'),
 ]
